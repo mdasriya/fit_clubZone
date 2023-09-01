da0fb30e64562ad671d8943b519264c4f5511b26
Mukesh Dasriya <dasriyamukesh87@gmail.com>
2023-09-01 14:05:21 +0530

    user Dashboard progress

--- screens/UserDashboardScreen.js
+++ screens/UserDashboardScreen.js
@@ -1,36 +1,52 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons'; // Import FontAwesome icons
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image, Radio } from 'react-native';
 
-const UserDashboardScreen = ({ navigation }) => {
-  const user = { name: 'John Doe' }; // Replace with the user's data
+const UserDashboardScreen = () => {
+  const user = { name: 'John Doe', profileImageUrl: 'https://images.pexels.com/photos/3764119/pexels-photo-3764119.jpeg?auto=compress&cs=tinysrgb&w=600' };
+  const [fitnessGoal, setFitnessGoal] = useState('');
+
+  const handleFitnessGoalChange = (goal) => {
+    setFitnessGoal(goal);
+  };
 
   return (
     <View style={styles.container}>
-      {/* Custom header */}
       <View style={styles.header}>
-        <FontAwesome name="user" size={24} color="white" style={styles.profileIcon} />
         <Text style={styles.headerText}>{user.name}</Text>
+        <Image source={{ uri: user.profileImageUrl }} style={styles.profileImage} />
       </View>
       
-      <Text style={styles.heading}>User Dashboard</Text>
-    
-
-      {/* Add placeholders for workout plans, nutrition plans, progress charts, achievements, etc. */}
-      <Text style={styles.placeholder}>Workout Plans</Text>
-      <Text style={styles.placeholder}>Nutrition Plans</Text>
-      <Text style={styles.placeholder}>Progress Charts</Text>
-      <Text style={styles.placeholder}>Achievements</Text>
+   
+      <Text style={styles.sectionHeading}>Select Fitness Goal:</Text>
+      <View style={styles.fitnessGoalContainer}>
+        <TouchableOpacity
+          style={[
+            styles.goalOption,
+            fitnessGoal === 'Weight Loss' && styles.goalOptionSelected,
+          ]}
+          onPress={() => handleFitnessGoalChange('Weight Loss')}
+        >
+          <Text style={styles.goalText}>Weight Loss</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.goalOption,
+            fitnessGoal === 'Muscle Gain' && styles.goalOptionSelected,
+          ]}
+          onPress={() => handleFitnessGoalChange('Muscle Gain')}
+        >
+          <Text style={styles.goalText}>Muscle Gain</Text>
+        </TouchableOpacity>
+        {/* Add more fitness goals as needed */}
+      </View>
+      
+      {/* Add workout progress chart */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+ 
   header: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -39,37 +55,46 @@ const styles = StyleSheet.create({
     right: 0,
     padding: 10,
   },
-  profileIcon: {
+  profileImage: {
+    width: 50,
+    height: 50,
+    marginLeft:10,
+    borderRadius: 25,
     marginRight: 10,
   },
   headerText: {
-    color: 'white',
+    color: 'black',
     fontSize: 18,
   },
   heading: {
     fontSize: 24,
     marginBottom: 20,
   },
-  profileButton: {
-    backgroundColor: 'blue',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
+  sectionHeading: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 20,
     marginBottom: 10,
   },
-  logButton: {
-    backgroundColor: 'green',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
+  fitnessGoalContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
+  },
+  goalOption: {
+    flex: 1,
+    backgroundColor: '#E0E0E0',
+    padding: 10,
     borderRadius: 5,
+    marginHorizontal: 5,
+    alignItems: 'center',
   },
-  buttonText: {
-    color: 'white',
-    fontSize: 18,
+  goalOptionSelected: {
+    backgroundColor: 'green',
   },
-  placeholder: {
+  goalText: {
+    color: 'black',
     fontSize: 16,
-    marginVertical: 10,
   },
 });
 
