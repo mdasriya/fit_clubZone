49ab638e032c898ab848d615008da58230e4ea1e
Mukesh Dasriya <dasriyamukesh87@gmail.com>
2023-09-01 09:16:12 +0530

    user Login part done

--- components/HomeScreen .js
+++ components/HomeScreen .js
@@ -1,14 +1,38 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Welcome to Fitness Club</Text>
-      <Button title="User Login" onPress={() => navigation.navigate('UserLogin')} />
-      <Button title="Trainer Login" onPress={() => navigation.navigate('TrainerLogin')} />
-    </View>
+  
+
+<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+<Text style={styles.heading}>Welcome to Fitness Club</Text>
+<View style={styles.buttonContainer}>
+<Button style={{marginBottom:"10px"}} title="User Login" onPress={() => navigation.navigate('UserLogin')} />
+<Button title="Trainer Login" onPress={() => navigation.navigate('TrainerLogin')} />
+</View>
+
+</View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    fontSize: 24, // Larger font size
+    marginBottom: 20, // Add margin at the bottom
+  },
+  buttonContainer: {
+    flexDirection: 'col', // Display buttons horizontally
+  },
+  buttonGap: {
+    width: 20, // Add gap between buttons
+    
+  },
+});
 
 export default HomeScreen;
--- screens/UserLoginScreen.js
+++ screens/UserLoginScreen.js
@@ -1,15 +1,145 @@
-import React from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 
 const UserLoginScreen = () => {
+  const [isButtonPressed, setIsButtonPressed] = useState(false);
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [gender, setGender] = useState('');
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
+  const [email, setEmail] = useState('');
+  const [contactNumber, setContactNumber] = useState('');
+
+  const genderOptions = [
+    { label: 'Select Gender', value: '' },
+    { label: 'Male', value: 'Male' },
+    { label: 'Female', value: 'Female' },
+    { label: 'Other', value: 'Other' },
+  ];
+
+  const handleButtonPress = () => {
+    setIsButtonPressed(true);
+    // Handle form submission logic here
+    console.log('Form submitted:', { name, age, gender, height, weight, email, contactNumber });
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>User Login</Text>
-      <TextInput placeholder="Name" />
-      {/* Add other form fields */}
-      <Button title="Login" onPress={() => console.log('User Logged In')} />
+    <View style={styles.container}>
+      <Text style={styles.heading}>User Login</Text>
+      <View style={styles.formContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Name"
+          value={name}
+          onChangeText={setName}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Age"
+          value={age}
+          onChangeText={setAge}
+          keyboardType="numeric"
+        />
+        {/* <View style={styles.pickerContainer}>
+          <RNPickerSelect
+            style={{ inputAndroid: styles.input }}
+            items={genderOptions}
+            placeholder={{ label: 'Select Gender', value: null }}
+            onValueChange={setGender}
+            value={gender}
+          />
+        </View> */}
+        <TextInput
+          style={styles.input}
+          placeholder="Height"
+          value={height}
+          onChangeText={setHeight}
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Gender"
+          value={gender}
+          onChangeText={setGender}
+  
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Weight"
+          value={weight}
+          onChangeText={setWeight}
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Contact Number"
+          value={contactNumber}
+          onChangeText={setContactNumber}
+          keyboardType="phone-pad"
+        />
+      </View>
+      <TouchableOpacity
+        style={[styles.button, isButtonPressed && styles.buttonPressed]}
+        onPress={handleButtonPress}
+      >
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  formContainer: {
+    width: '80%',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  pickerContainer: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    justifyContent: 'center', // Center picker vertically
+  },
+  button: {
+    backgroundColor: 'blue',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonPressed: {
+    backgroundColor: 'darkblue', // Change color when pressed
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+  },
+});
 
 export default UserLoginScreen;
